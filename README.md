fcc135d82b37642d44e59f40f1519a72fe3034a1
likkradyus <perl{at}li.que.jp>
2017-07-04 18:03:55 +0900

    initial import.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Undead - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Undead;
+
+# DESCRIPTION
+
+Acme::Undead is ...
+
+# LICENSE
+
+Copyright (C) likkradyus.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+likkradyus &lt;perl{at}li.que.jp>
---
+++ lib/Acme/Undead.pm
@@ -0,0 +1,39 @@
+package Acme::Undead;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Undead - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Undead;
+
+=head1 DESCRIPTION
+
+Acme::Undead is ...
+
+=head1 LICENSE
+
+Copyright (C) likkradyus.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+likkradyus E<lt>perl{at}li.que.jpE<gt>
+
+=cut
+
